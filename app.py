5f5dce981421da33a6c3845e46ed9b8383a4deec
Pushparaj N <160032689+Stormorigins@users.noreply.github.com>
2025-03-11 09:41:45 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #load libraies
 
 import numpy as np
-import tensorflow as tf 
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import load_model
